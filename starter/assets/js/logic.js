930becf46bef843ae3f7051fc1001dccda0d6788
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-01-13 18:34:34 +0000

    -added more questions

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,27 +1,3 @@
-var quiz = [
-  {
-    title: "Arrays in JavaScript ca be used to store _______.",
-    choices: [
-      "1. numbers and strings",
-      "2. other arrays",
-      "3. booleans",
-      " 4. all of the above",
-    ],
-    answer: "all of the above",
-  },
-  {
-    title:
-      "A vey useful tool used during development and debbugging for printing content to the debugger is: ",
-    choices: [
-      "1. JavaScript",
-      "2. terminal/bash",
-      "3. for loops",
-      "4. console.log",
-    ],
-    answer: "console.log",
-  },
-];
-
 // html id convert in variables
 var timer = document.querySelector("#time");
 var startScreen = document.querySelector("#start-screen");
---
+++ starter/assets/js/questions.js
@@ -0,0 +1,44 @@
+var quiz = [
+  {
+    title: "Arrays in JavaScript ca be used to store _______.",
+    choices: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      " 4. all of the above",
+    ],
+    answer: "4. all of the above",
+  },
+  {
+    title:
+      "A vey useful tool used during development and debbugging for printing content to the debugger is: ",
+    choices: [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loops",
+      "4. console.log",
+    ],
+    answer: "4. console.log",
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alers",
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parentheses",
+      "4. square brackets",
+    ],
+    answer: "3. parentheses",
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    answer: "3. quotes",
+  },
+];
